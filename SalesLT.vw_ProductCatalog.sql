04f83fe476a0e3bbbe1e94a336933f039180a384
Hikmatyor Azatov <azatovhikmatyor@gmail.com>
2026-07-24 22:19:44 +0500

    new file created

---
+++ SalesLT.vw_ProductCatalog.sql
@@ -0,0 +1,25 @@
+CREATE VIEW SalesLT.vw_ProductCatalog
+AS
+SELECT
+    p.ProductID,
+    p.Name AS ProductName,
+    p.ProductNumber,
+    p.Color,
+    p.ListPrice,
+    p.Size,
+    p.Weight,
+    pc.Name AS CategoryName,
+    COALESCE(parent.Name, 'Uncategorized') AS ParentCategoryName,
+    pm.Name AS ModelName,
+    pmd.Description AS ModelDescription
+FROM SalesLT.Product p
+INNER JOIN SalesLT.ProductCategory pc
+    ON p.ProductCategoryID = pc.ProductCategoryID
+LEFT JOIN SalesLT.ProductCategory parent
+    ON pc.ParentProductCategoryID = parent.ProductCategoryID
+LEFT JOIN SalesLT.ProductModel pm
+    ON p.ProductModelID = pm.ProductModelID
+LEFT JOIN SalesLT.ProductModelProductDescription pmd
+    ON pm.ProductModelID = pmd.ProductModelID
+    AND pmd.Culture = 'en'
+WHERE p.SellEndDate IS NULL
